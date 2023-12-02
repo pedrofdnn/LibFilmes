003c80a96af570f3b699a45800f24a1c2d240c6e
Pedro Fernandes <pedrofdnn@gmail.com>
2023-12-02 18:17:55 -0300

    função do enter e click concluído

--- src/Components/NavbarComponet.tsx
+++ src/Components/NavbarComponet.tsx
@@ -1,15 +1,46 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { NavContainer, Searchbar } from "../Styles/StyleNav";
+import { LiaSearchSolid } from "react-icons/lia";
+
+// import getTopRatedMovies from "../API/API";
 
 export default function NavbarComponent() {
+  const [changeClick, setChangeClick] = useState("");
+
+  // evento de checagem do enter e click
+  function handleEnter(e: React.KeyboardEvent<HTMLInputElement>) {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      alert(`O valor digitado foi: ${changeClick}`);
+    }
+  }
+  function handleClick() {
+    alert(`O valor digitado foi: ${changeClick}`);
+  }
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setChangeClick(e.target.value);
+  }
+
   return (
     <NavContainer>
       <nav>
         <Link to="/">Home</Link>
+
         <Searchbar>
-          <input type="text" />
-          <button type="submit">P</button>
+          <input
+            type="text"
+            placeholder="Pesquise o Filme"
+            value={changeClick}
+            onChange={handleChange}
+            onKeyDown={handleEnter}
+          />
+
+          <button onClick={handleClick}>
+            <LiaSearchSolid />
+          </button>
         </Searchbar>
+
         <Link to="/contact">Contato</Link>
       </nav>
     </NavContainer>
--- src/Pages/SearchPage.tsx
+++ src/Pages/SearchPage.tsx
@@ -4,7 +4,7 @@ export default function SearchPage() {
   return (
     <div>
       <h1>Resultado da pesquisa.</h1>
-      {/* <SearchComponent searchTerm={query || ""} /> */}
+      
     </div>
   );
 }
