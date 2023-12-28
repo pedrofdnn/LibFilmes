50a3344ea3ac34bff963a442eb021b3549578c5a
Pedro Fernandes <85568352+pedrofdnn@users.noreply.github.com>
2023-12-27 22:05:33 -0300

    criando pagina de apresentação

--- src/Pages/Contact.tsx
+++ src/Pages/Contact.tsx
@@ -1,9 +1,27 @@
-
 export default function ContactPage() {
   return (
     <div>
       <h1>Contato</h1>
-      <p>Pagina de informações para contato</p>
+
+      <span>
+        <div>
+          <img
+            src="https://avatars.githubusercontent.com/u/85568352?v=4"
+            alt="Imagem de Apresentação"
+          />
+        </div>
+        <h2>Olá me chamo Pedro Fernandes.</h2>
+        <p>
+          Sou formado pelo bootcamp da Labenu e atualmente estou aprimorando
+          meus conhecimentos na faculdade, onde curso Ciências da Computação.
+          Tenho 10 anos de experiência com suporte técnico, onde realizava
+          suporte direto ao usuário presencialmente e remotamente. Hoje estou em
+          transição de carreira para ser um desenvolvedor e estou buscando
+          oportunidade na nova área com o objetivo de poder participar de
+          soluções que possam impactar na qualidade de vida das pessoas,
+          tornando tarefas e obrigações mais simples.
+        </p>
+      </span>
     </div>
   );
 }
---
+++ src/Styles/StyleContac.ts
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+const ContainerContact = styled.span`
+
+
+`;
